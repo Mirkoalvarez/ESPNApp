c32181ee48b516ee32b4d2b0039bb18eea68271c
Mirko Alvarez <mirko.bolivar2003@gmail.com>
2025-10-05 20:42:17 -0300

    nameproject

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pokeapi"
+    namespace = "com.example.espnapp"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.pokeapi"
+        applicationId = "com.example.espnapp"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
--- app/src/main/java/com/example/pokeapi/ui/results/ResultsUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pokeapi.ui.results
-
-sealed class ResultsUiState {
-    object Idle : ResultsUiState()
-    object Loading : ResultsUiState()
-    data class Success(val items: List<ScoreItem>) : ResultsUiState()
-    data class Empty(val message: String) : ResultsUiState()
-    data class Error(val message: String) : ResultsUiState()
-}
